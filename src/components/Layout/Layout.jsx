c134b8ed01011ae3dd591c5ce863b0350a536ad5
Izhar <izharramdan93@gmail.com>
2025-03-22 21:48:33 +0700

    add activity section

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,13 +3,12 @@ import { Outlet } from "react-router-dom";
 import Navbar from "./Header/Index";
 import Footer from "./Footer/Index";
 
-
 const Layout = () => {
   return (
     <div
       className="flex flex-col min-h-screen bg-cover bg-center"
       style={{
-        backgroundImage: `url('https://images.unsplash.com/photo-1507608616759-54f48f0af0ee?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`,
+        backgroundImage: `url('https://images.unsplash.com/photo-1517495306984-f84210f9daa8?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`,
       }}
     >
       <Navbar />
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
         <PromoSection />
         <BannerSection />
         <CategorySection />
-        {/* <ActivitySection /> */}
+        <ActivitySection />
       </div>
     </>
   );
