cc8d0857cd619ba4dab2b51990e3e5d02b514472
Izhar <izharramdan93@gmail.com>
2025-04-11 22:08:24 +0700

    update activity

--- src/pages/Activity/Index.jsx
+++ src/pages/Activity/Index.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Card, Typography } from "@material-tailwind/react";
 import useActivity from "../../components/Views/Home/hooks/useActivity";
+import { useNavigate } from "react-router-dom";
 
 const Activity = () => {
   const { activities, loading, error } = useActivity();
+  const navigate = useNavigate();
   return (
     <div className="container mx-auto mt-8 px-16">
       <Typography variant="h2" className="text-center mb-8">
@@ -11,7 +13,11 @@ const Activity = () => {
       </Typography>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {activities.map((activity) => (
-          <Card key={activity.id} className="relative flex flex-col h-full">
+          <Card
+            key={activity.id}
+            className="relative flex flex-col h-full cursor-pointer"
+            onClick={() => navigate(`/activity/${activity.id}`)}
+          >
             <div
               className="h-48 bg-cover bg-center rounded-t-lg"
               style={{ backgroundImage: `url(${activity.imageUrls[0]})` }}
